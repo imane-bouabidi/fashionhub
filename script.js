c790026026a6ec34f340fa8c359cbcfcf1871f4c
imane-bouabidi <imaneyoucode@gmail.com>
2023-10-23 20:43:28 +0100

    version2

---
+++ script.js
@@ -0,0 +1,23 @@
+const inputs = document.querySelectorAll('input');
+
+const patterns ={
+    telephone: /^\d{11}$/,
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})$/,
+}
+
+function validate(field, regex) {
+    if(regex.test(field.value)) {
+        field.className='valid';
+    }else{
+        field.className='invalid';
+    }
+}
+
+inputs.forEach((input)=> {
+    input.addEventListener('keyup',(e)=> {
+        validate(e.target,patterns[e.target.attributes.name.value])
+    });
+});
+
