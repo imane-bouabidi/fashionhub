b6f50776e3db90bdbee2cba725870a776c9774bf
imane-bouabidi <imaneyoucode@gmail.com>
2023-11-22 10:30:02 +0100

    version finale

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const patterns ={
     password: /^[\w\W@-]{8,20}$/,
     email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})$/i,
 }
-console.log(inputs)
 
 function validate(field, regex) {
     if(regex.test(field.value)) {
